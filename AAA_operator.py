bfa8538e68b287a711cb9d287aa3453311f522e3
onth0s <metaagnosis@gmail.com>
2025-01-23 23:25:34 +0100

    just in case

---
+++ AAA_operator.py
@@ -0,0 +1,262 @@
+import bpy
+import os
+import re
+from datetime import datetime
+
+from bpy.props import (FloatProperty, IntProperty,
+                       BoolProperty, StringProperty)
+from bpy.types import (Menu, Operator)
+from bl_operators.presets import AddPresetBase
+from mathutils import *
+from math import *
+
+from AAA_var import *
+
+
+''' Notes
+    I dont know how to set up the poll() function
+
+    bpy.ops.info.reports_display_update()
+    doesnt work as expected. well, it doesnt work at all
+    I need to register the operator and get some flood if i wanna update the Info Editor
+'''
+
+# if you call it from other operator the self.report() function prints to the console instead of the Info Editor, independently of the 'execution context'
+
+
+class SaveIncremental(Operator):
+    bl_idname = "aaa.save_incremental"
+    bl_label = ""
+    bl_options = {'REGISTER'}
+
+    def execute(self, context):
+        currentblend = bpy.data.filepath
+        if currentblend:
+            save_path = self.get_incremented_path(currentblend)
+            # self.add_path_to_recent_files(save_path)
+            if os.path.exists(save_path):
+                self.report(
+                    {'INFO'}, "File '%s' exists already!\nBlend has NOT been saved incrementally!" % (save_path))
+            else:
+                bpy.ops.wm.save_as_mainfile(filepath=save_path)
+                self.report({'INFO'}, "Saved blend incrementally:" + save_path)
+        else:
+            bpy.ops.wm.save_mainfile('INVOKE_DEFAULT')
+        return {'FINISHED'}
+
+    def get_incremented_path(self, currentblend):
+        path = os.path.dirname(currentblend)
+        filename = os.path.basename(currentblend)
+
+        filenameRegex = re.compile(r"(.+)\.blend\d*$")
+
+        mo = filenameRegex.match(filename)
+
+        if mo:
+            name = mo.group(1)
+            numberendRegex = re.compile(r"(.*?)(\d+)$")
+
+            mo = numberendRegex.match(name)
+
+            if mo:
+                basename = mo.group(1)
+                numberstr = mo.group(2)
+            else:
+                basename = name + "_"
+                numberstr = "000"
+
+            number = int(numberstr)
+
+            incr = number + 1
+            incrstr = str(incr).zfill(len(numberstr))
+            incrname = basename + incrstr + ".blend"
+
+            return os.path.join(path, incrname)
+
+    def add_path_to_recent_files(self, path):
+        """
+        add the path to the recent files list, for some reason it's not done automatically when saving or loading
+        """
+
+        try:
+            recent_path = bpy.utils.user_resource('CONFIG', "recent-files.txt")
+            with open(recent_path, "r+") as f:
+                content = f.read()
+                f.seek(0, 0)
+                f.write(path.rstrip('\r\n') + '\n' + content)
+
+        except (IOError, OSError, FileNotFoundError):
+            pass
+
+
+class SaveFile(Operator):
+    bl_idname = "aaa.save_file"
+    bl_label = "saveFile"
+    bl_options = {'REGISTER'}
+
+    def execute(self, context):
+        filename = bpy.path.basename(bpy.context.blend_data.filepath)
+        bpy.ops.wm.save_mainfile('INVOKE_DEFAULT')
+        if bpy.data.is_saved:
+            if bpy.data.is_dirty:
+                saved = "Saved: " + filename
+                self.report({'INFO'}, saved)
+
+                bpy.data.scenes[0].already_saved_counter = 0
+            else:
+                bpy.data.scenes[0].already_saved_counter += 1
+                st = "No changes have been made to '" + filename + \
+                    "'. Already saved file (" + \
+                    str(bpy.data.scenes[0].already_saved_counter) + ")"
+                self.report({'INFO'}, st)
+        return {'FINISHED'}
+
+
+class TestOperator(Operator):
+    bl_idname = "aaa.test_operator"
+    bl_label = ""
+    bl_options = {'REGISTER', 'UNDO'}
+
+    testVal: bpy.props.IntProperty()
+
+    def execute(self, context):
+        time = str(datetime.time(datetime.now()))
+        print("{}: {}".format(self.testVal, time[:-7]))
+
+        return {'FINISHED'}
+
+
+''' Preset Operator
+class test(Operator):
+    bl_idname = "aaa.test"
+    bl_label = ""
+    bl_options = {'REGISTER', 'UNDO'}
+    def execute(self, context):
+        
+        return {'FINISHED'}
+'''
+classes = (
+
+    TestOperator,
+
+    GLOBAL_ALT_S,
+    GLOBAL_ALT_Q,
+    GLOBAL_ALT_A,
+
+    GLOBAL_CTRL_ALT_H,
+    GLOBAL_CTRL_F,
+
+    GLOBAL_SHIFT_CTRL_F,
+
+    GLOBAL_SHIFT_ALT_Z,
+
+    GLOBAL_SHIFT_ALT_F,
+    GLOBAL_SHIFT_Q,
+    GLOBAL_SHIFT_E,
+    GLOBAL_SHIFT_A,
+
+    GLOBAL_W,
+    GLOBAL_Q,
+    GLOBAL_E,
+
+    CONDITIONS_SWITCHER_SEQUENCER,
+    CONDITIONS_SWITCHER,
+
+    PresetFrameRangePreviewMove,
+    PresetFrameRangePreviewOverwrite,
+    PresetFrameRangePreviewRemove,
+    PresetFrameRangePreviewAdd,
+
+    # PresetPanelInfo,
+    PresetOverlays,
+    PresetBackground,
+
+    SetProp,
+    ToggleProp,
+
+    PanelGPBrushesManagerRefresh,
+    PanelInfoShow,
+
+    OnaddGPObject,
+    OnCallGPLayersPanel,
+
+    SelectReferenceImage,
+
+    SaveFile,
+    SaveIncremental,
+
+    ApplyModifiers,
+
+    SetBaseFPS,
+
+    HelperGPBrushesManager,
+
+    GPSetStrokesOpacity,
+
+    GPSwitchMaterial,
+
+    GPRemoveTool,
+
+    GPSetTool1,
+    GPSetTool2,
+    GPSetTool3,
+    GPSetTool4,
+    GPSetTool5,
+    GPSetTool6,
+    GPSetTool7,
+    GPSetTool8,
+
+    GPSwitchTool,
+
+    GPLayerUnlockedMovetoSelected,
+    GPLayerUnlockedMove,
+    GPLayerClear,
+    GPLayerResetOpacity,
+    GPLayerDuplicateHide,
+    GPLayerSeparateSelection,
+    GPLayerDelete,
+    GPLayerMergeDown,
+    GPLayerNew,
+
+    ModeGPSwitcher,
+
+    SwitchCavityType,
+
+    RendererPicker,
+
+    DyntopoDetailing,
+
+    CommonTools,
+    PivotSelector,
+
+    ModeSet,
+
+    RollAxis,
+    RollViewport,
+
+    ToggleFaceOrientation,
+    ToggleXRay,
+    ToggleWireframeOverlay,
+    ToggleSolidWireframe,
+    ToggleOverlays,
+
+    SwitchWorkspace,
+
+    VSECustomFadeClear,
+    VSECustomFade,
+    VSEAlignStrip,
+)
+
+
+def register():
+    for c in classes:
+        bpy.utils.register_class(c)
+
+
+def unregister():
+    for c in classes:
+        bpy.utils.unregister_class(c)
+
+
+if __name__ == "__main__":
+    register()
--- AAA_pie.py
+++ AAA_pie.py
@@ -1,7 +1,7 @@
-import bpy # type: ignore
-from bpy.types import Menu # type: ignore
+import bpy  # type: ignore
+from bpy.types import Menu  # type: ignore
 
-# 'VIEW3D_' is not necessary, it just looks cleaner this way, 
+# 'VIEW3D_' is not necessary, it just looks cleaner this way,
 # because it's the context in which you shall call it
 
 ''' Useful API ENUMS
@@ -20,9 +20,11 @@ from bpy.types import Menu # type: ignore
         GPENCIL, CAMERA, LIGHT, SPEAKER, LIGHT_PROBE], default EMPTY
     '''
 
+
 class PIE_MT_KEY_CONDITIONS(Menu):
     bl_idname = "PIE_MT_KEY_CONDITIONS"
     bl_label = "Conditions"
+
     def draw(self, context):
         pie = self.layout.menu_pie()
 
@@ -35,16 +37,50 @@ class PIE_MT_KEY_CONDITIONS(Menu):
         pie.operator('wm.call_menu', text="")
         pie.operator('wm.call_menu', text="")
 
+
+class PIE_MT_SAVE_N_STUFF(Menu):
+    bl_idname = "PIE_MT_SAVE_N_STUFF"
+    bl_label = "Save N' Stuff"
+
+    def draw(self, context):
+        layout = self.layout
+        pie = layout.menu_pie()
+
+        if context.area.type == 'TEXT_EDITOR':
+            pie.operator("text.run_script", text="Run Script")
+        else:
+            pie.operator("script.reload", text="Reload Scripts")
+
+        pie.operator("aaa.save_file", text="Save")
+
+        pie.operator_context = 'INVOKE_DEFAULT'
+        pie.operator("wm.open_mainfile", text="Open")
+
+        pie.operator("wm.save_homefile", text="Override Startup")
+        pie.operator("import_scene.obj", text="Import OBJ")
+
+        # leave 'app_template' in blank to load the 'startup' file
+        pie.operator("wm.append", text="Append")
+        pie.operator("wm.read_homefile", text="New File").app_template = ""
+        pie.operator("aaa.save_file", text="[...]")
+        pie.operator("aaa.save_incremental", text="Save Incremental")
+
+
 classes = (
     PIE_MT_KEY_CONDITIONS,
+    PIE_MT_SAVE_N_STUFF,
 )
+
 
 def register():
     for c in classes:
         bpy.utils.register_class(c)
+
+
 def unregister():
     for c in classes:
         bpy.utils.unregister_class(c)
+
 
 if __name__ == "__main__":
     register()
