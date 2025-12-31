eed4817cff796c915556c2b03636c797458105f3
onth0s <metaagnosis@gmail.com>
2025-12-31 06:09:09 +0100

    this stupid shit took way longer than it should've

--- AAA_operator.py
+++ AAA_operator.py
@@ -195,13 +195,15 @@ class ToggleOverlays(Operator):
                 SD.show_gizmo              = False
                 SD.show_region_ui          = False
                 SD.show_region_toolbar     = False
-                SD.show_region_asset_shelf = False
+                if context.mode == MHS:
+                    SD.show_region_asset_shelf = False
             else:
                 SD.overlay.show_overlays = SN.show_overlays
                 SD.show_gizmo = SN.show_gizmo
                 SD.show_region_ui = SN.show_t_menu
                 SD.show_region_toolbar = SN.show_n_menu
-                SD.show_region_asset_shelf = SN.show_region_asset_shelf
+                if context.mode == MHS:
+                    SD.show_region_asset_shelf = SN.show_region_asset_shelf
 
             SN.show_bool_toggle = not SN.show_bool_toggle
 
